0607e70fdcb04756443a5c65327cc1af824e3ea4
Andrusi4ka <Andrusi4ka1985@gmail.com>
2025-12-11 01:01:27 +0100

    Update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -2,7 +2,7 @@ const Gallery = () => {
   return (
     <div>
         <h1 className="text-center">Gallery</h1>
-        <img className="w-100" src="../../public/155-3264x2176.jpg" alt="155-3264x2176.jpg" />
+        <img className="w-100" src="https://picsum.photos/id/155/3264/2176" alt="155-3264x2176.jpg" />
     </div>
   )
 }
